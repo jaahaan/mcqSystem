8f2b77787147e39dc925352e9fed80626dd1d36e
Nargish Jahan <86972304+jaahaan@users.noreply.github.com>
2022-06-29 16:09:14 +0600

    Add files via upload

---
+++ question.js
@@ -0,0 +1,30 @@
+var arr = [
+    {
+        q: "Which of these is the correct way in which we can call the JavaScript code?",
+        a: "Triggering Event",
+        b: "Preprocesso",
+        c: "Function/Method",
+        ans: "ans3"
+    },
+    {
+        q: "Which of these is the correct way in which we can call the JavaScript code?",
+        a: "Triggering Event",
+        b: "Preprocesso",
+        c: "Function/Method",
+        ans: "ans3"
+    },
+    {
+        q: "Which of these is the correct way in which we can call the JavaScript code?",
+        a: "Triggering Event",
+        b: "Preprocesso",
+        c: "Function/Method",
+        ans: "ans3"
+    }
+];
+
+const question = document.querySelector('#question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+
+
--- timer.js
+++ timer.js
@@ -1,3 +1,69 @@
+var arr = [
+    {
+        q: "What is JavaScript?",
+        a: "Markup Language",
+        b: "Class-based Object Oriented LanguagePreprocesso",
+        c: "Prototype based Object Oriented Language",
+        ans: "ans3"
+    },
+    {
+        q: "Which of these is the correct way in which we can call the JavaScript code?",
+        a: "Triggering Event",
+        b: "Preprocesso",
+        c: "Function/Method",
+        ans: "ans3"
+    },
+    {
+        q: "Which of these is known as the Equality operator used for checking whether both the values are equal?",
+        a: "=",
+        b: "==",
+        c: "===",
+        ans: "ans2"
+    },
+    {
+        q: "Which of these is known as the Equality operator used for checking whether both the values are equal?",
+        a: "=",
+        b: "==",
+        c: "===",
+        ans: "ans2"
+    }
+];
+
+const question = document.querySelector('#question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const next = document.getElementById('next');
+const answers = document.querySelectorAll('.answer');
+
+let questionCnt = 0;
+var score = 0;
+
+const loadQuestion = () => {
+    const questionList = arr[questionCnt];
+    question.innerHTML = questionList.q;
+    option1.innerHTML = questionList.a;
+    option2.innerHTML = questionList.b;
+    option3.innerHTML = questionList.c;
+}
+
+
+loadQuestion();
+
+const checkAns = () => {
+    let answer;
+    answers.forEach((curAnsElem) => {
+        if (curAnsElem.checked) {
+            answer = curAnsElem.id;
+        }
+    });
+    return answer;
+};
+
+const deselect = () => {
+    answers.forEach((curAnsElem) => curAnsElem.checked = false);
+}
+
 var sec = 0;
 var min = 0;
 
@@ -9,16 +75,57 @@ function start() {
     document.getElementById("clock").style.display = "block";
     var x = setInterval(incSec, 1000);
     var y = setInterval(incMin, 60000);
-
     document.getElementById("submit").addEventListener("click", stop);
+
+    next.addEventListener('click', () => {
+        const checkedAns = checkAns();
+
+        if (checkedAns === arr[questionCnt].ans) {
+            score++;
+        }
+        questionCnt++;
+        deselect();
+        if (questionCnt < arr.length) {
+            loadQuestion();
+        } else {
+            if (score < 2) {
+                document.getElementById("box").style.display = "none";
+                document.getElementById("clock").style.display = "none";
+                document.getElementById("danger").style.display = "block";
+                document.getElementById("scored").innerHTML = "Your Score: " + score;
+                document.getElementById("durationd").innerHTML = "Your Duration: " + min + " : " + sec;
+            } else {
+                document.getElementById("box").style.display = "none";
+                document.getElementById("clock").style.display = "none";
+                document.getElementById("success").style.display = "block";
+                document.getElementById("score").innerHTML = "Your Score: " + score;
+                document.getElementById("duration").innerHTML = "Your Duration: " + min + " : " + sec;
+            }
+
+        }
+    });
     function stop() {
+        const checkedAns = checkAns();
+
+        if (checkedAns === arr[questionCnt].ans) {
+            score++;
+        }
         clearInterval(x);
         clearInterval(y);
 
-        document.getElementById("box").style.display = "none";
-        document.getElementById("clock").style.display = "none";
-        document.getElementById("success").style.display = "block";
-        document.getElementById("score").innerHTML = "Duration: " + min + " : " + sec;
+        if (score < 2) {
+            document.getElementById("box").style.display = "none";
+            document.getElementById("clock").style.display = "none";
+            document.getElementById("danger").style.display = "block";
+            document.getElementById("scored").innerHTML = "Your Score: " + score;
+            document.getElementById("durationd").innerHTML = "Your Duration: " + min + " : " + sec;
+        } else {
+            document.getElementById("box").style.display = "none";
+            document.getElementById("clock").style.display = "none";
+            document.getElementById("success").style.display = "block";
+            document.getElementById("score").innerHTML = "Your Score: " + score;
+            document.getElementById("duration").innerHTML = "Your Duration: " + min + " : " + sec;
+        }
     }
 }
 
